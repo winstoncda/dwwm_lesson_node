16817fe695189872d3a9653ccb763f8fd5cdc3dd
winstoncda <olivier.pro.cda@gmail.com>
2025-08-25 16:28:38 +0200

    login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,20 @@
 import User from "../models/user.schema.js";
+import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-
     const user = await User.findOne({ email });
+    const secondUser = await User.findOne({ username });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (user) {
+    if (user || secondUser) {
       res.status(400).json({ message: "Déjà inscrit" });
     }
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
     await newUser.save();
     res.status(200).json({ message: "Utilisateur enregistré" });
@@ -22,5 +24,30 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { data, password } = req.body;
   console.log(req.body);
+
+  let user;
+
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if (emailRegex.test(data)) {
+    user = await User.findOne({ email: data });
+  } else {
+    user = await User.findOne({ username: data });
+  }
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Email ou nom d'utilisateur incorrect" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: "Mot de passe incorrect" });
+  }
+
+  // Si tout est bon
+  res.status(200).json({ user, message: "Connexion réussie" });
 };
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
